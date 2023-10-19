5551c48fd5a02b92f8995b966dfd13dd3d2d664e
Duilia Guerrero <dui.2809@gmail.com>
2023-10-19 16:18:54 -0500

    CLI

--- AppFront/src/app/components/users/users-list/users-list.component.ts
+++ AppFront/src/app/components/users/users-list/users-list.component.ts
@@ -5,7 +5,7 @@ import { inject } from '@angular/core';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  
 })
 export class UsersListComponent {
 
